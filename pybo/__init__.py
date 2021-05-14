7634d2e57c11e1aa40aa026e4e169e84a549f201
효진 <0103leehj@yonsei.ac.kr>
2021-05-14 14:54:25 +0900

    pybo.py 추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -8,29 +8,27 @@ import config
 db=SQLAlchemy()
 migrate= Migrate()
 
-def create_app():
-    app= Flask(__name__)
 
-    #sqlalcehmy db 설정. 순서 중요.
-    #1)
-    app.config.from_object(config) #config 연결
-    db.init_app(app)
-    migrate.init_app(app,db)
-    #2)
-    from . import models
-    #3)
-    from .views import main_views, naver_views, question_views, answer_views, auth_views, naver_movie
-    app.register_blueprint(main_views.bp) #앱에다 등록시키는 이 과정이 있어야 함.
-    app.register_blueprint(naver_views.bp) #naver.py에서도 bp=Blureprint라고 변수정의했어서 .bp
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(naver_movie.bp)
+app= Flask(__name__)
 
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime']=format_datetime
+#sqlalcehmy db 설정. 순서 중요.
+#1)
+app.config.from_object(config) #config 연결
+db.init_app(app)
+migrate.init_app(app,db)
+#2)
+from . import models
+#3)
+from .views import main_views, naver_views, question_views, answer_views, auth_views, naver_movie
+app.register_blueprint(main_views.bp) #앱에다 등록시키는 이 과정이 있어야 함.
+app.register_blueprint(naver_views.bp) #naver.py에서도 bp=Blureprint라고 변수정의했어서 .bp
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(naver_movie.bp)
 
-    return app
+from .filter import format_datetime
+app.jinja_env.filters['datetime']=format_datetime
 
 #플라스크로 웹서버만들때. 패키지 만들기(파이썬 폴더 패키지) 후 views(디렉토리)
 
